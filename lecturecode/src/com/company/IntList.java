744b07a2951fd31a3ad276684cd4c8d4d862fc7a
Jose Cisneros <jmc@asu.edu>
2020-12-18 15:52:47 -0700

    move main method into IntList

--- lecturecode/src/com/company/IntList.java
+++ lecturecode/src/com/company/IntList.java
@@ -64,4 +64,15 @@ public class IntList {
         }
         return  L;
     }
+
+    public static void main(String[] args) {
+        IntList L = new IntList(15, null);
+        L = new IntList(10, L);
+        L = new IntList(5, L);
+
+        L.printList();
+        IntList squared = IntList.squareMutative(L);
+        squared.printList();
+        L.printList();
+    }
 }
