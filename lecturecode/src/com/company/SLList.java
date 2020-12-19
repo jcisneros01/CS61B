fe1b62a5f5ad3350e6d7e8b4d000e13c36258958
Jose Cisneros <jmc@asu.edu>
2020-12-18 17:52:17 -0700

    add new methods

--- lecturecode/src/com/company/SLList.java
+++ lecturecode/src/com/company/SLList.java
@@ -13,16 +13,41 @@ public class SLList {
 
     private IntNode first;
 
+    private int size;
+
     public SLList(int x) {
         first = new IntNode(x, null);
+        size = 1;
     }
 
     public void addFirst(int x) {
         first = new IntNode(x, first);
+        size += 1;
     }
 
     public int getFirst() {
         return first.item;
+    }
+
+    public void addLast(int x) {
+        IntNode p = first;
+        while (p.next != null) {
+            p = p.next;
+        }
+
+        p.next = new IntNode(x, null);
+        size += 1;
+    }
+
+    public int size() {
+        return size (first);
+    }
+
+    private int size(IntNode p) {
+        if (p == null) {
+            return 1;
+        }
+        return 1 + size(p.next);
     }
 
     public static void main(String[] args) {
