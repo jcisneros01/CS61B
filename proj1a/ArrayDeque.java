f006ab0e1ba9468a445e1af9b7018cb0d73aebf6
Jose Cisneros <jmc@asu.edu>
2020-12-29 22:39:08 -0700

    add ArrayDeque skeleton

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,10 +1,22 @@
-public class ArrayDeque {
-    //    public void addFirst(T item) {}
-//    public void addLast(T item) {}
-//    public boolean isEmpty() {}
-//    public int size() {}
-//    public void printDeque(){}
-//    public T removeFirst() {}
-//    public T removeLast() {}
-//    public T get(int index){}
+public class ArrayDeque<T> {
+
+    public ArrayDeque(){};
+    public void addFirst(T item) {}
+    public void addLast(T item) {}
+    public boolean isEmpty() {
+        return true;
+    }
+    public int size() {
+        return 0;
+    }
+    public void printDeque(){}
+    public T removeFirst() {
+        return null;
+    }
+    public T removeLast() {
+        return null;
+    }
+    public T get(int index){
+        return null;
+    }
 }
