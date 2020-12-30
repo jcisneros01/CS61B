551c4c22d2836561b0b8b06b963dcc4b5f13a9b2
Jose Cisneros <jmc@asu.edu>
2020-12-29 19:19:05 -0700

    finish LLD and add tests

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,10 +1,121 @@
 public class LinkedListDeque<T> {
-//    public void addFirst(T item) {}
-//    public void addLast(T item) {}
-//    public boolean isEmpty() {}
-//    public int size() {}
-//    public void printDeque(){}
-//    public T removeFirst() {}
-//    public T removeLast() {}
-//    public T get(int index){}
+    private class Node {
+        public T item;
+        public Node prev;
+        public Node next;
+
+        public Node(T item) {
+            this.item = item;
+        }
+
+        public Node(Node prev, Node Next, T item) {
+            this.prev = prev;
+            next = Next;
+            this.item = item;
+        }
+    }
+
+    private final Node sentinel;
+
+    private int size;
+
+    public LinkedListDeque() {
+        sentinel = new Node(null);
+        size = 0;
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+    }
+
+    public void addFirst(T item) {
+        Node node;
+        if (size == 0) {
+            node = new Node(sentinel, sentinel, item);
+            sentinel.next = node;
+            sentinel.prev = node;
+        } else {    
+            node = new Node(sentinel, sentinel.next, item);
+            sentinel.next.prev = node;
+            sentinel.next = node;
+        }
+        
+        size++;
+    }
+
+    public void printDeque() {
+        Node p = sentinel.next;
+        int count = size;
+        while (count > 0) {
+            System.out.printf("%s ", p.item);
+            p = p.next;
+            count--;
+        }
+    }
+
+   public void addLast(T item) {
+        if (isEmpty()) {
+            addFirst(item);
+            return;
+        }
+        Node last = sentinel.prev;
+        Node newLast = new Node(last, sentinel, item);
+        last.next = newLast;
+        sentinel.prev = newLast;
+        size++;
+   }
+
+    public int size() {
+
+        return size;
+    }
+
+    public T removeFirst() {
+        if (isEmpty()) {
+            return null;
+        }
+
+        Node node = sentinel.next;
+        sentinel.next = node.next;
+        size--;
+        return node.item;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public T get(int index){
+        if (index > size - 1) {
+            return null;
+        }
+
+        Node p = sentinel.next;
+        int count = index;
+        while (count > 0) {
+            p = p.next;
+            count--;
+        }
+        return p.item;
+    }
+
+    public T removeLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        Node last = sentinel.prev;
+
+        Node newLast = last.prev;
+        newLast.next = sentinel;
+        sentinel.prev = newLast;
+        size--;
+
+        return last.item;
+    }
+
+    public static void main(String[] args) {
+        LinkedListDeque<Integer> L = new LinkedListDeque<>();
+        L.addFirst(10);
+        L.addFirst(9);
+        L.addFirst(8);
+        L.printDeque();
+    }
 }
---
+++ proj1a/ListTests.java
@@ -0,0 +1,132 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ListTests {
+    @Test
+    public void IsSize_ListIsEmpty_ReturnsTrue() {
+        var list = new LinkedListDeque<Integer>();
+
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void IsSize_ListIsNotEmpty_ReturnsFalse() {
+        var list = new LinkedListDeque<Integer>();
+        list.addFirst(1);
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void Size_EmptyList_ReturnsZero() {
+        var list = new LinkedListDeque<Integer>();
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void Size_HasOneValue_ReturnsOne() {
+        var list = new LinkedListDeque<Integer>();
+        list.addFirst(1);
+        assertEquals(1, list.size());
+    }
+
+
+    @Test
+    public void Get_AllConditions_ReturnsItem() {
+        var list = new LinkedListDeque<Integer>();
+        list.addFirst(10);
+        list.addFirst(9);
+        list.addFirst(8);
+        assertEquals(8, (int) list.get(0));
+        assertEquals(9, (int) list.get(1));
+        assertEquals(10, (int) list.get(2));
+    }
+
+    @Test
+    public void Get_BeyondIndex_ReturnsNull() {
+        var list = new LinkedListDeque<Integer>();
+        list.addFirst(10);
+        list.addFirst(9);
+        list.addFirst(8);
+        assertNull(list.get(3));
+    }
+
+    @Test
+    public void Get_EmptyList_ReturnsNull() {
+        var list = new LinkedListDeque<Integer>();
+        assertEquals(null, list.get(0));
+    }
+
+    @Test
+    public void AddFirst_EmptyList_AddedItemToFront() {
+        var list = new LinkedListDeque<Integer>();
+        list.addFirst(10);
+        assertEquals(10, (int) list.get(0));
+    }
+
+    @Test
+    public void AddFirst_AddMultipleItems_AddedItemToFront() {
+        var list = new LinkedListDeque<Integer>();
+        list.addFirst(10);
+        list.addFirst(9);
+        list.addFirst(8);
+        assertEquals(8, (int) list.get(0));
+    }
+
+    @Test
+    public void RemoveFirst_EmptyList_ReturnsNull() {
+        var list = new LinkedListDeque<Integer>();
+        assertNull(list.removeFirst());
+    }
+
+    @Test
+    public void RemoveFirst_NonEmptyList_RemovesItems() {
+        var list = new LinkedListDeque<Integer>();
+        list.addFirst(10);
+        list.addFirst(9);
+        list.addFirst(8);
+        int first = list.removeFirst();
+        int second = list.removeFirst();
+        int third = list.removeFirst();
+        var fourth = list.removeFirst();
+        assertEquals(8, first);
+        assertEquals(9, second);
+        assertEquals(10, third);
+        assertNull(fourth);
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void RemoveLast_NonEmptyList_RemovesItems() {
+        var list = new LinkedListDeque<Integer>();
+        list.addFirst(10);
+        list.addFirst(9);
+        list.addFirst(8);
+        int first = list.removeLast();
+        int second = list.removeLast();
+        int third = list.removeLast();
+        var fourth = list.removeLast();
+        assertEquals(10, first);
+        assertEquals(9, second);
+        assertEquals(8, third);
+        assertNull(fourth);
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void AddLast_EmptyList_AddedItemToFront() {
+        var list = new LinkedListDeque<Integer>();
+        list.addLast(10);
+        assertEquals(10, (int) list.get(0));
+    }
+
+    @Test
+    public void AddLast_AddMultipleItems_AddedItemToFront() {
+        var list = new LinkedListDeque<Integer>();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.printDeque();
+       assertEquals(3, (int) list.get(2));
+    }
+}
