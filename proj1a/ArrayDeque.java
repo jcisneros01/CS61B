31a44b3f136917584edf38a4fa208bae39ced7e8
Jose Cisneros <jmc@asu.edu>
2020-12-31 18:48:09 -0700

    1st take

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,39 +1,97 @@
 public class ArrayDeque<T> {
+    private T[] items;
+
+    private int nextFirst;
+
+    private int nextLast;
+
+    private int size;
 
     public ArrayDeque() {
+        items =  (T[]) new Object[8];
+        size = 0;
+        nextFirst = 4;
+        nextLast = 5;
+    }
 
+    private int minusOne(int index) {
+        index--;
+        return (index + items.length) % items.length;
+    }
+
+    private int plusOne(int index) {
+        index++;
+        return (index + items.length) % items.length;
     }
 
     public void addFirst(T item) {
-
+        items[nextFirst] = item;
+        size++;
+        nextFirst = minusOne(nextFirst);
     }
 
     public void addLast(T item) {
-
+        items[nextLast] = item;
+        size++;
+        nextLast = plusOne(nextLast);
     }
 
     public boolean isEmpty() {
-        return true;
+        return size == 0;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public void printDeque() {
-
+        for (int i = 0; i < size ; i++) {
+            System.out.printf("%s ", get(i));
+        }
     }
 
     public T removeFirst() {
-        return null;
+        if (size == 0) {
+            return null;
+        }
+
+        var item = items[plusOne(nextFirst)];
+        items[plusOne(nextFirst)] = null;
+        nextFirst = plusOne(nextFirst); // move up pointer
+        size--;
+
+        return item;
     }
 
     public T removeLast() {
-        return null;
+        if (size == 0) {
+            return null;
+        }
+
+        var item = items[minusOne(nextLast)];
+        items[minusOne(nextLast)] = null;
+        nextLast = minusOne(nextLast); // move pointer down
+        size--;
+
+        return item;
     }
 
     public T get(int index) {
+        var firstIndex = plusOne(nextFirst);
+        index = index + firstIndex;
+        index = (index + items.length) % items.length;
+        return items[index];
+    }
 
-        return null;
+    public static void main(String[] args)
+    {
+        var deq = new ArrayDeque<Character>();
+        deq.addLast('a');
+        deq.addLast('b');
+        deq.addFirst('c');
+        deq.addLast('d');
+        deq.addLast('e');
+        deq.addFirst('f');
+        deq.printDeque();
     }
 }
---
+++ proj1a/ArrayDequeTests.java
@@ -0,0 +1,131 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ArrayDequeTests {
+    @Test
+    public void isSizeListIsEmptyReturnsTrue() {
+        var list = new ArrayDeque<Integer>();
+
+        assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void isSizeListIsNotEmptyReturnsFalse() {
+        var list = new ArrayDeque<Integer>();
+        list.addFirst(1);
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void sizeEmptyListReturnsZero() {
+        var list = new ArrayDeque<Integer>();
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void sizeHasOneValueReturnsOne() {
+        var list = new ArrayDeque<Integer>();
+        list.addFirst(1);
+        assertEquals(1, list.size());
+    }
+
+    @Test
+    public void getAllConditionsReturnsItem() {
+        var list = new ArrayDeque<Integer>();
+        list.addFirst(10);
+        list.addFirst(9);
+        list.addFirst(8);
+        assertEquals(8, (int) list.get(0));
+        assertEquals(9, (int) list.get(1));
+        assertEquals(10, (int) list.get(2));
+    }
+
+    @Test
+    public void getBeyondIndexReturnsNull() {
+        var list = new ArrayDeque<Integer>();
+        list.addFirst(10);
+        list.addFirst(9);
+        list.addFirst(8);
+        assertNull(list.get(3));
+    }
+
+    @Test
+    public void getEmptyListReturnsNull() {
+        var list = new ArrayDeque<Integer>();
+        assertEquals(null, list.get(0));
+    }
+
+    @Test
+    public void addFirstEmptyListAddedItemToFront() {
+        var list = new ArrayDeque<Integer>();
+        list.addFirst(10);
+        assertEquals(10, (int) list.get(0));
+    }
+
+    @Test
+    public void addFirstAddMultipleItemsAddedItemToFront() {
+        var list = new ArrayDeque<Integer>();
+        list.addFirst(10);
+        list.addFirst(9);
+        list.addFirst(8);
+        assertEquals(8, (int) list.get(0));
+    }
+
+    @Test
+    public void removeFirstEmptyListReturnsNull() {
+        var list = new ArrayDeque<Integer>();
+        assertNull(list.removeFirst());
+    }
+
+    @Test
+    public void removeFirstNonEmptyListRemovesItems() {
+        var list = new ArrayDeque<Integer>();
+        list.addFirst(10);
+        list.addFirst(9);
+        list.addFirst(8);
+        int first = list.removeFirst();
+        int second = list.removeFirst();
+        int third = list.removeFirst();
+        var fourth = list.removeFirst();
+        assertEquals(8, first);
+        assertEquals(9, second);
+        assertEquals(10, third);
+        assertNull(fourth);
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void removeLastNonEmptyListRemovesItems() {
+        var list = new ArrayDeque<Integer>();
+        list.addFirst(10);
+        list.addFirst(9);
+        list.addFirst(8);
+        int first = list.removeLast();
+        int second = list.removeLast();
+        int third = list.removeLast();
+        var fourth = list.removeLast();
+        assertEquals(10, first);
+        assertEquals(9, second);
+        assertEquals(8, third);
+        assertNull(fourth);
+        assertEquals(0, list.size());
+    }
+
+    @Test
+    public void addLastEmptyListAddedItemToFront() {
+        var list = new ArrayDeque<Integer>();
+        list.addLast(10);
+        assertEquals(10, (int) list.get(0));
+    }
+
+    @Test
+    public void addLastAddMultipleItemsAddedItemToFront() {
+        var list = new ArrayDeque<Integer>();
+        list.addLast(1);
+        list.addLast(2);
+        list.addLast(3);
+        list.printDeque();
+        assertEquals(3, (int) list.get(2));
+    }
+}
