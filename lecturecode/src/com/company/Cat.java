fc9e220319b75df99f06ec2213499beded1871c7
Jose Cisneros <jmc@asu.edu>
2021-02-15 01:59:20 -0700

    add animal and cat classes

---
+++ lecturecode/src/com/company/Cat.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Cat extends Animal {
+
+    public Cat(String name, int age) {
+        super(name, age);
+        noise = "Meow!";
+    }
+
+    @Override
+    public void greet() {
+        System.out.println("Cat " + name + " says: " + makeNoise());
+    }
+}
