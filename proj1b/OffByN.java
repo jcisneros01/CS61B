37cfaae9f8ffd7d9239323b829d23a4865010cd3
Jose Cisneros <jmc@asu.edu>
2021-02-20 00:03:29 -0700

    add OffByN

---
+++ proj1b/OffByN.java
@@ -0,0 +1,12 @@
+public class OffByN implements CharacterComparator{
+    private int _n;
+    OffByN(int N) { 
+        _n = N;
+    }
+    
+    @Override
+    public boolean equalChars(char x, char y) {
+        int characterDifference  = Math.abs(x - y);
+        return characterDifference == _n;
+    }
+}
