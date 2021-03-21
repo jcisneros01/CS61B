434923e130b2b5c4f5ce467fea6fba6cad364fd7
Jose Cisneros <jmc@asu.edu>
2021-03-21 00:08:38 -0700

    Update ArrayRingBuffer.java

--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -68,6 +68,10 @@ public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T>  {
      * Return oldest item, but don't remove it.
      */
     public T peek() {
+        if (isEmpty()) {
+            throw new RuntimeException("Ring buffer underflow");
+        }
+
         return rb[first];
     }
 
